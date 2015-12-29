53cf56a80d17dd2c5fb8dafa435858e56d09cd6c
Chaz <cmtlum73@gmail.com>
2015-12-29 13:37:55 -1000

    added gulp sass live-reload and browserify

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var browserify = require('gulp-browserify');
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
+
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+  return gulp.src('./scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./public/**/*', ['livereload']);
+});
+
+gulp.task('browserify', function() {
+  // Single entry point to browserify
+  gulp.src('./js/*.js')
+    .pipe(browserify({
+      insertGlobals : true,
+      debug : !gulp.env.production
+    }))
+    .pipe(gulp.dest('./js/build'));
+});
+
+gulp.task('default', ['connect', 'watch', 'sass', 'browserify']);
